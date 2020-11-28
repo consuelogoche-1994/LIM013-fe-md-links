84635b21580d69fe91a9f4edffbd59e24f6707e9
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-11-27 22:12:23 -0500

    Import module fs and create (isdirectory, isfile, readDIr and readfile) - close #6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 // verificar si path es absoluto
 const isAbsolute = (route) => path.isAbsolute(route);
@@ -6,9 +7,24 @@ const isAbsolute = (route) => path.isAbsolute(route);
 const transformRelative = (route) => path.resolve(route);
 // verificar si extensión es .md
 const isMdFile = (route) => (path.extname(route) === '.md');
+// verificar si es file
+const isFile = (route) => fs.lstatSync(route).isFile();
+// verificar si es directorio
+const isDirectory = (route) => fs.statSync(route).isDirectory();
+// verificar si directorio o documento existe
+const routeExists = (route) => fs.existsSync(route);
+// extraer el array todos lo contenido en primer nivel
+const readDir = (route) => fs.readdirSync(route);
+// extraer el array todos lo contenido en primer nivel
+const readFile = (route) => fs.readFileSync(route, 'utf8');
 
 module.exports = {
   isAbsolute,
+  isFile,
+  isDirectory,
+  routeExists,
   isMdFile,
+  readDir,
+  readFile,
   transformRelative,
 };
